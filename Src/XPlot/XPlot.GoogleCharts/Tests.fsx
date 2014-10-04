e8827890338ce0f3922103aa3055b5b7c34bf0ed
Taha Hachana <tahahachana@gmail.com>
2014-10-04 11:24:56 +0100

    Stacked bar chart test

--- Src/XPlot/XPlot.GoogleCharts/Tests.fsx
+++ Src/XPlot/XPlot.GoogleCharts/Tests.fsx
@@ -137,4 +137,25 @@ module Bar =
         |> Chart.WithLegend true
         |> Chart.Show
 
+    // stacked
+    let chart4 =
+        let fantasy = ["2010", 10; "2020", 16; "2030", 28]
+        let romance = ["2010", 24; "2020", 22; "2030", 19]
+        let mystery = ["2010", 20; "2020", 23; "2030", 29]
+        let general = ["2010", 32; "2020", 30; "2030", 30]
+        let western = ["2010", 18; "2020", 16; "2030", 12]
+        let literature = ["2010", 5; "2020", 9; "2030", 13]
 
+        let options =
+            Options(
+                width = 600,
+                height = 400,
+                legend = Legend(position = "top", maxLines = 3),
+                bar = Bar(groupWidth = "75%"),
+                isStacked = true
+            )
+        [fantasy; romance; mystery; general; western; literature]
+        |> Chart.Bar
+        |> Chart.WithOptions options
+        |> Chart.WithLabels ["Fantasy & Sci Fi"; "Romance"; "Mystery/Crime"; "General"; "Western"; "Literature"]
+        |> Chart.Show
