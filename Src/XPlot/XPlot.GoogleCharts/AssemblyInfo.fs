cb9b45bb1dd2c54499ad519214802a6d02c593f4
Taha Hachana <tahahachana@gmail.com>
2014-10-23 08:49:36 +0100

    Publish version 0.9

--- Src/XPlot/XPlot.GoogleCharts/AssemblyInfo.fs
+++ Src/XPlot/XPlot.GoogleCharts/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 
 [<
     AssemblyDescription("F# Data Visualization Library");
-    AssemblyFileVersion("0.8.0");
+    AssemblyFileVersion("0.9.0");
     AssemblyProduct("XPlot");
     AssemblyTitle("XPlot");
-    AssemblyVersion("0.8.0")
+    AssemblyVersion("0.9.0")
 >]
 
 do ()
