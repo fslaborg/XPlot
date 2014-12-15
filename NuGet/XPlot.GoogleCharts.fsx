00217084ed7c1531c1d9bc4033940bfaddb7fd96
Taha Hachana <tahahachana@gmail.com>
2014-12-15 13:58:12 +0100

    Fix #3

--- NuGet/XPlot.GoogleCharts.fsx
+++ NuGet/XPlot.GoogleCharts.fsx
@@ -10,3 +10,36 @@ open XPlot.GoogleCharts
 |> Chart.Area
 |> fun chart -> chart.Html
 |> ignore
+
+(*
+
+Adds add a DWORD registry value to specify which
+rendering mode and version of IE should be used by FSI.
+
+**Problem**: WinForms and WPF browser controls default to IE7
+standards mode.
+
+**Solution**: Instruct FSI to use the IE11 Standards mode
+and prevent Google Charts from falling back to VML.
+
+*)
+
+open Microsoft.Win32
+
+let subKeyPath = @"SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION"
+let keyName = "FsiAnyCPU.exe"
+let localMachine = Registry.LocalMachine
+
+try
+    let keyExists =
+        let subKey = localMachine.OpenSubKey(subKeyPath)
+        subKey.GetValueNames()
+        |> Array.exists (fun x -> x = keyName)
+
+    match keyExists with
+    | false ->
+        let key = localMachine.CreateSubKey(subKeyPath, RegistryKeyPermissionCheck.ReadWriteSubTree)
+        key.SetValue(keyName, 11001, RegistryValueKind.DWord)
+        key.Close()
+    | true -> ()
+with _ -> printfn "Failed to instruct FSI to use the IE11 Standards mode."
