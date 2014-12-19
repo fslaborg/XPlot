f3e0fec5408f96cd201bed6a1828e1c5a6fe12cd
Taha Hachana <tahahachana@gmail.com>
2014-12-19 13:55:32 +0100

    Column chart helpers for Deedle

--- Src/XPlot/XPlot.GoogleCharts.Deedle/Main.fs
+++ Src/XPlot/XPlot.GoogleCharts.Deedle/Main.fs
@@ -106,3 +106,39 @@ type Chart with
         let dt = data.ToDataTable(["Key"])
         let options = defaultArg Options <| Configuration.Options()
         GoogleChart.CreateFromDataTable dt options ChartGallery.Candlestick
+
+    /// <summary>Creates a column chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Labels">Labels for the data table columns.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Column(data:Series<'K, 'V>, ?Labels, ?Options) =
+        let series =
+            data
+            |> Series.observations
+            |> Seq.map Datum.New
+            |> Series.New
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.Create [series] Labels options ChartGallery.Column
+
+    /// <summary>Creates a column chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Labels">Labels for the data table columns.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Column(data:seq<Series<'K, 'V>> when 'K :> key and 'V :> value, ?Labels, ?Options) =
+        let series =
+            data
+            |> Seq.map (fun x ->
+                x 
+                |> Series.observations
+                |> Seq.map Datum.New
+                |> Series.New)
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.Create series Labels options ChartGallery.Column
+
+    /// <summary>Creates a column chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Column(data:Frame<'K, 'V>, ?Options) =
+        let dt = data.ToDataTable(["Key"])
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.CreateFromDataTable dt options ChartGallery.Column
