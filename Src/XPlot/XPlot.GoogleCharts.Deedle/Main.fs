9c4943752c4776548ac9a7eee99bf315c396c873
Taha Hachana <tahahachana@gmail.com>
2014-12-19 08:05:16 +0100

    Bar chart helpers for Deedle

--- Src/XPlot/XPlot.GoogleCharts.Deedle/Main.fs
+++ Src/XPlot/XPlot.GoogleCharts.Deedle/Main.fs
@@ -36,9 +36,44 @@ type Chart with
 
     /// <summary>Creates an area chart.</summary>
     /// <param name="data">The chart's data.</param>
-    /// <param name="Labels">Labels for the data table columns.</param>
     /// <param name="Options">The chart's options.</param>
     static member Area(data:Frame<'K, 'V>, ?Options) =
         let dt = data.ToDataTable(["Key"])
         let options = defaultArg Options <| Configuration.Options()
         GoogleChart.CreateFromDataTable dt options Area
+
+    /// <summary>Creates a bar chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Labels">Labels for the data table columns.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Bar(data:Series<'K, 'V>, ?Labels, ?Options) =
+        let series =
+            data
+            |> Series.observations
+            |> Seq.map Datum.New
+            |> Series.New
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.Create [series] Labels options ChartGallery.Bar
+
+    /// <summary>Creates a bar chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Labels">Labels for the data table columns.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Bar(data:seq<Series<'K, 'V>> when 'K :> key and 'V :> value, ?Labels, ?Options) =
+        let series =
+            data
+            |> Seq.map (fun x ->
+                x 
+                |> Series.observations
+                |> Seq.map Datum.New
+                |> Series.New)
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.Create series Labels options ChartGallery.Bar
+
+    /// <summary>Creates a bar chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Bar(data:Frame<'K, 'V>, ?Options) =
+        let dt = data.ToDataTable(["Key"])
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.CreateFromDataTable dt options ChartGallery.Bar
--- Src/XPlot/XPlot.GoogleCharts.Deedle/Tests.fsx
+++ Src/XPlot/XPlot.GoogleCharts.Deedle/Tests.fsx
@@ -55,3 +55,65 @@ module Area =
         |> Chart.WithOptions options
         |> Chart.WithLegend true
         |> Chart.Show
+
+module Bar =
+
+    let sales = series ["2013" => 1000; "2014" => 1170; "2015" => 660; "2016" => 1030]
+    let expenses = series ["2013" => 400; "2014" => 460; "2015" => 1120; "2016" => 540]
+
+    let chart1 =
+        sales
+        |> Chart.Bar
+        |> Chart.WithTitle "Company Sales"
+        |> Chart.WithXTitle "Year"
+        |> Chart.WithYTitle "Amount"
+        |> Chart.Show
+
+    let chart2 =
+        [sales; expenses]
+        |> Chart.Bar
+        |> Chart.WithLabels ["Sales"; "Expenses"]
+        |> Chart.WithTitle "Company Performance"
+        |> Chart.WithXTitle "Year"
+        |> Chart.WithYTitle "Amount"
+        |> Chart.WithLegend true
+        |> Chart.Show
+
+    let options = Options()
+
+    options.title <- "Company Performance"
+
+    let vAxisOptions = Axis()
+    vAxisOptions.title <- "Year"
+    vAxisOptions.titleTextStyle <- TextStyle(color = "red")
+    options.vAxis <- vAxisOptions
+
+    let chart3 =
+        ["Sales" => sales; "Expenses" => expenses]
+        |> Frame.ofColumns
+        |> Chart.Bar
+        |> Chart.WithOptions options
+        |> Chart.WithLegend true
+        |> Chart.Show
+
+    let chart4 =
+        let fantasy = ["2010" => 10; "2020" => 16; "2030" => 28]
+        let romance = ["2010" => 24; "2020" => 22; "2030" => 19]
+        let mystery = ["2010" => 20; "2020" => 23; "2030" => 29]
+        let general = ["2010" => 32; "2020" => 30; "2030" => 30]
+        let western = ["2010" => 18; "2020" => 16; "2030" => 12]
+        let literature = ["2010" => 5; "2020" => 9; "2030" => 13]
+
+        let options =
+            Options(
+                width = 600,
+                height = 400,
+                legend = Legend(position = "top", maxLines = 3),
+                bar = Bar(groupWidth = "75%"),
+                isStacked = true
+            )
+        [fantasy; romance; mystery; general; western; literature]
+        |> Chart.Bar
+        |> Chart.WithOptions options
+        |> Chart.WithLabels ["Fantasy & Sci Fi"; "Romance"; "Mystery/Crime"; "General"; "Western"; "Literature"]
+        |> Chart.Show
