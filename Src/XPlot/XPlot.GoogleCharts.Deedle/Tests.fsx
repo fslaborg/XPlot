f63b57a71e006c76631118779daa5c2e31af08bf
Taha Hachana <tahahachana@gmail.com>
2014-12-20 08:48:38 +0100

    Line chart helpers for Deedle

--- Src/XPlot/XPlot.GoogleCharts.Deedle/Tests.fsx
+++ Src/XPlot/XPlot.GoogleCharts.Deedle/Tests.fsx
@@ -462,3 +462,43 @@ module Histogram =
         |> Chart.WithOptions options
         |> Chart.WithLabel "Length"
         |> Chart.Show
+
+module Line =
+
+    let sales = series ["2013", 1000; "2014", 1170; "2015", 660; "2016", 1030]
+    let expenses = series ["2013", 400; "2014", 460; "2015", 1120; "2016", 540]
+    
+    let chart1 =
+        sales
+        |> Chart.Line
+        |> Chart.Show
+
+    let chart2 =
+        [sales; expenses]
+        |> Chart.Line
+        |> Chart.WithTitle "Company Performance"
+        |> Chart.WithLabels ["Sales"; "Expenses"]
+        |> Chart.WithLegend true
+        |> Chart.Show
+        
+    let chart3 =
+        let options =
+            Options(
+                title = "Company Performance",
+                curveType = "function",
+                legend = Legend(position = "bottom")
+            )
+        [sales; expenses]
+        |> Chart.Line
+        |> Chart.WithOptions options
+        |> Chart.WithLabels ["Sales"; "Expenses"]
+        |> Chart.Show
+
+    let chart4 =
+        ["Sales" => sales; "Expenses" => expenses]
+        |> Frame.ofColumns
+        |> Chart.Line
+        |> Chart.WithTitle "Company Performance"
+        |> Chart.WithLegend true
+        |> Chart.Show
+
