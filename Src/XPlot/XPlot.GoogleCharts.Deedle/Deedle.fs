c18e08d1715628773cdcfc17bcf995d909edd40e
Taha Hachana <tahahachana@gmail.com>
2014-12-20 13:39:12 +0100

    Pie chart helpers for Deedle

--- Src/XPlot/XPlot.GoogleCharts.Deedle/Deedle.fs
+++ Src/XPlot/XPlot.GoogleCharts.Deedle/Deedle.fs
@@ -285,3 +285,16 @@ type Chart with
         let dt = data.ToDataTable(["Key"])
         let options = defaultArg Options <| Configuration.Options()
         GoogleChart.CreateFromDataTable dt options ChartGallery.Map
+
+    /// <summary>Creates a pie chart.</summary>
+    /// <param name="data">The chart's data.</param>
+    /// <param name="Labels">Labels for the data table columns.</param>
+    /// <param name="Options">The chart's options.</param>
+    static member Pie(data:Series<string, #value>, ?Labels, ?Options) =
+        let series =
+            data
+            |> Series.observations
+            |> Seq.map Datum.New
+            |> Series.New
+        let options = defaultArg Options <| Configuration.Options()
+        GoogleChart.Create [series] Labels options ChartGallery.Pie
--- Src/XPlot/XPlot.GoogleCharts.Deedle/Tests.fsx
+++ Src/XPlot/XPlot.GoogleCharts.Deedle/Tests.fsx
@@ -552,3 +552,74 @@ module Map =
         |> Chart.WithOptions options
         |> Chart.Show
 
+module Pie =
+
+    let chart1 =
+        [
+            "Work", 11
+            "Eat", 2
+            "Commute", 2
+            "Watch TV", 2
+            "Sleep", 7
+        ]
+        |> series
+        |> Chart.Pie
+        |> Chart.WithTitle "My Daily Activities"
+        |> Chart.WithLegend true
+        |> Chart.Show
+
+    let chart2 =
+        let options =
+            Options(
+                title = "My Daily Activities",
+                is3D = true
+            )
+        [
+            "Work", 11
+            "Eat", 2
+            "Commute", 2
+            "Watch TV", 2
+            "Sleep", 7
+        ]
+        |> series
+        |> Chart.Pie
+        |> Chart.WithOptions options
+        |> Chart.WithLegend true
+        |> Chart.Show
+
+    let chart3 =
+        let options =
+            Options(
+                title = "My Daily Activities",
+                pieHole = 0.4
+            )
+        [
+            "Work", 11
+            "Eat", 2
+            "Commute", 2
+            "Watch TV", 2
+            "Sleep", 7
+        ]
+        |> series
+        |> Chart.Pie
+        |> Chart.WithOptions options
+        |> Chart.WithLegend true
+        |> Chart.Show
+
+    let chart4 =
+        let options =
+            Options(
+                pieSliceText = "label",
+                title = "Swiss Language Use (100 degree rotation)",
+                pieStartAngle = 100
+            )
+        [
+            "German", 5.85
+            "French", 1.66
+            "Italian", 0.316
+            "Romansh", 0.0791        
+        ]
+        |> series
+        |> Chart.Pie
+        |> Chart.WithOptions options
+        |> Chart.Show
