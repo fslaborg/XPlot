75fb48c8dc33f7d7116d418ec5f098cd8f61936f
Taha <tahahachana@outlook.com>
2016-04-23 15:03:49 +0100

    Plotly time series chart test

---
+++ src/XPlot.Plotly/Tests/TimeSeries.fsx
@@ -0,0 +1,16 @@
+﻿#r @"../../../bin/XPlot.Plotly.dll"
+
+open XPlot.Plotly
+
+// Date Strings
+module Chart1 =
+
+    let data =
+        Scatter(
+            x = ["2013-10-04 22:23:00"; "2013-11-04 22:23:00"; "2013-12-04 22:23:00"],
+            y = [1; 3; 6]
+        )
+
+    data
+    |> Plotly.Plot
+    |> Plotly.Show
