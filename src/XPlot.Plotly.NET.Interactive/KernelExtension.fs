7bf0d6c5cdae8238fd0b0ceef045b4bfed42aafa
Diego Colombo <dicolomb@microsoft.com>
2021-02-05 00:43:27 +0000

    start
    add extension method
    
    
    update tools
    
    
    updated build
    
    
    update Fsharp.Formatting.CommandTool
    
    
    update test project
    
    
    update tfm
    
    
    installed updates
    
    
    move to right folder
    
    
    add powershell kernel logic
    
    
    wip

---
+++ src/XPlot.Plotly.NET.Interactive/KernelExtension.fs
@@ -0,0 +1,68 @@
+﻿namespace XPlot.Plotly.NET.Interactive
+
+open System
+open System.Text
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore.Html
+open Microsoft.DotNet.Interactive
+open Microsoft.DotNet.Interactive.Formatting
+open Microsoft.DotNet.Interactive.Http
+open XPlot.Plotly
+
+open Microsoft.DotNet.Interactive.PowerShell
+open Microsoft.PowerShell
+open Microsoft.PowerShell.Commands
+
+open Giraffe.ViewEngine
+
+type KernelExtension() =
+    let getScriptElementWithRequire (script: string) =
+        let newScript = StringBuilder()
+        newScript.AppendLine("""<script type="text/javascript">""") |> ignore
+        newScript.AppendLine("""
+var renderPlotly = function() {
+    var xplotRequire = require.config({context:'xplot-3.0.1',paths:{plotly:'https://cdn.plot.ly/plotly-1.49.2.min'}}) || require;
+    xplotRequire(['plotly'], function(Plotly) { """) |> ignore
+        newScript.AppendLine(script) |> ignore
+        newScript.AppendLine(@"});
+};"
+        ) |> ignore
+        newScript.AppendLine(JavascriptUtilities.GetCodeForEnsureRequireJs(Uri("https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js"), "renderPlotly")) |> ignore
+        newScript.ToString()
+
+    let getHtml (chart: PlotlyChart) =
+        let styleStr = sprintf "width: %dpx; height: %dpx;" chart.Width chart.Height
+        let divElem =
+            div [_style styleStr; _id chart.Id] [] |> RenderView.AsString.htmlDocument
+
+        let js = chart.GetInlineJS().Replace("<script>", String.Empty).Replace("</script>", String.Empty)
+        HtmlString(divElem + getScriptElementWithRequire js)
+
+    let registerPlotlyFormatters () =
+        Formatter.Register<PlotlyChart>(
+            Action<_,_>(fun (chart: PlotlyChart) (writer: TextWriter) ->
+                writer.Write(getHtml chart)),
+            HtmlFormatter.MimeType)
+
+    let registerAccelerators () = 
+        ignore
+
+    let registerModule () = 
+        ignore
+
+    let configurePowerShellLernel () = 
+        registerAccelerators() |> ignore
+        registerModule() |> ignore
+        Task.CompletedTask        
+
+    interface IKernelExtension with
+        member _.OnLoadAsync (kernel: Kernel) =
+            registerPlotlyFormatters()
+            match kernel with
+            | :? PowerShellKernel as _powerShellKernel -> configurePowerShellLernel()
+            | _ -> Task.CompletedTask
+            
+
+    
---
+++ src/XPlot.Plotly.NET.Interactive/XPlotExtensions.fs
@@ -0,0 +1,13 @@
+﻿namespace XPlot.Plotly.NET.Interactive
+
+open Microsoft.DotNet.Interactive
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type XplotExtensions =
+    [<Extension>]
+    static member inline UseXplot<'a when 'a :> Kernel>(kernel: 'a) : 'a =
+        let extension = KernelExtension() :> IKernelExtension
+        extension.OnLoadAsync(kernel) |> ignore
+        kernel
+
